83c40d9d8bbbb765dbea0e3f4ffdcf46cf2caf5c
deadlock-mv <abmath374@gmail.com>
2024-01-12 17:47:05 +0530

    stripe payment - done

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const cors = require("cors");
 const dotenv = require("dotenv");
+
 const userRoute = require("./routes/user");
 const authRoute = require("./routes/auth");
 const productRoute = require("./routes/product");
 const cartRoute = require("./routes/cart");
 const orderRoute = require("./routes/order");
-
+const stripeRoute = require("./routes/stripe");
 
 dotenv.config();
 
@@ -15,20 +17,22 @@ mongoose.connect(process.env.MONGO_URL)
 .then(()=>{console.log("DB connection Successful")})
 .catch((err)=>{console.log(err)});
 
-app.listen(3000, ()=> {
-    console.log("Backend Server is running");
-})
-
-app.use(express.json());
-
 app.get("/api/test", ()=>{
     console.log("Testing");
 });
 
+app.use(express.json());
+app.use(cors());
 app.use("/api/user", userRoute);
 app.use("/api/auth", authRoute);
 app.use("/api/products", productRoute);
 app.use("/api/cart", cartRoute);
 app.use("/api/orders", orderRoute);
+app.use("/api/checkout", stripeRoute);
+
+
+app.listen(3000, ()=> {
+    console.log("Backend Server is running");
+})
 
 
